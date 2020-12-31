188ac9b7af30e2c8c4960d2a6de078d0ed149665
Maxwell Newman <maxwellsawesome@gmail.com>
2020-12-30 20:20:43 -0500

    redirect to https

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ clients = 0
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use (function (req, res, next) {
+  if (req.secure) {
+          // request was via https, so do no special handling
+          next();
+  } else {
+          // request was via http, so redirect to https
+          res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
